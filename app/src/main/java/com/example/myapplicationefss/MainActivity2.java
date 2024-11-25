ed5aefd52c1802b32624133f02c4dfaf676ff5eb
Настя <vetkav89@gmail.com>
2024-11-25 10:25:52 +0300

    2 задание 2 этап добавление 2 экрана

--- app/src/main/java/com/example/myapplicationefss/MainActivity2.java
+++ app/src/main/java/com/example/myapplicationefss/MainActivity2.java
@@ -3,8 +3,6 @@ package com.example.myapplicationefss;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,32 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView helloworld;
-    private ImageButton startButton2;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        helloworld = (TextView) findViewById(R.id.helloworld);
-        startButton2 = (ImageButton) findViewById(R.id.startButton2);
     }
-
-    public void onStartButtonClick(View view) {
-        startButton2.setImageResource(R.drawable.android);
-        helloworld.setVisibility(View.VISIBLE);
-        startButton2.setClickable(false);
-    }
-    public void mainclick(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+    public void button5(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
